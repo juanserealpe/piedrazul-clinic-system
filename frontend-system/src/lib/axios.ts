664ad09a1a68f5d57b889300488b783bd7f0b23d
juanserealpe <juansevarona@hotmail.com>
2026-06-08 18:43:46 -0500

    fix : env changed

--- frontend-system/src/lib/axios.ts
+++ frontend-system/src/lib/axios.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:"http://localhost:3000"
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
